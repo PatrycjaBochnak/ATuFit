5225a3706fad8abb347cce8237301c04eae4aae8
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-08 19:51:42 +0100

    creating homepage

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function HomePage () {
     return(
-        <div><span>Hello</span></div>
+        <>
+        <div><h1><span>Welcome to A Tu Fit, a website application works like Fitatu mobile app</span></h1></div>
+        <div><h2><span>A step-by-step guide through the process of creating your diet</span></h2></div>
+        <div><h3><span>Our team has developed a simple and easy to use diet diary builder. It is powered by a constantly expanding range of new and proven products.</span></h3></div>
+        </>
     )
 };
 
