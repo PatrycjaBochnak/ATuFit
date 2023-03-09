873a0603bed986b7fcbcb6316818bee310ac59c5
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-09 22:54:33 +0100

    change

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -5,7 +5,7 @@ function HomePage () {
         <>
         <div className="headings-homePage">
         <img alt="pic" src="../images/logo"/>
-        <div className="heading-1"><h3><span>Welcome to A Tu Fit</span></h3></div>
+        <div className="heading-1"><h3><span>Welcome to A Tu Fit website</span></h3></div>
         <div className="heading-2"><h2><span>A step-by-step guide through the process of creating your diet</span></h2></div>
         <div className="heading-3"><p><span>Our team has developed a simple and easy to use diet diary builder. It is powered by a constantly expanding range of new and proven products.</span></p></div>
         </div>
