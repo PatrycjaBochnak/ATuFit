aff822cd66f7eb190085a1a05baba4c4fd02c03a
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-23 20:15:16 +0100

    repairing code style

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import './App.scss';
-import CaloriesCounter from './components/caloriesCounter';
-import NavBar from './components/navBar';
+import React from "react";
+import "./App.scss";
+import CaloriesCounter from "./components/caloriesCounter";
+import NavBar from "./components/navBar";
 import Contact from "./components/contact";
-import HomePage from "./components/homePage"
-import CalculatorBMR from './components/calculatorBMR';
-import {
-    Routes, Route, Outlet
-} from "react-router-dom";
+import HomePage from "./components/homePage";
+import Calculator from "./components/calculatorBMR";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 const Layout = () => {
-    return <NavBar><Outlet /></NavBar>
-}
+  return (
+    <NavBar>
+      <Outlet />
+    </NavBar>
+  );
+};
 
 class App extends React.Component {
-    state = {
-    product: ''
-    };
+  state = {
+    product: "",
+  };
 
-    setCurrentProduct = (product) => {
-    this.setState({product: product});
-    };
+  setCurrentProduct = (product) => {
+    this.setState({ product: product });
+  };
 
-    render() {
-        return (
-            <Routes>
-                <Route element={ <Layout /> } >
-                    <Route path="/" element={ < HomePage /> } />
-                    <Route path="/caloriesCounter" element={ < CaloriesCounter /> } />
-                    <Route path="/calculator" element={ < CalculatorBMR /> } /> 
-                    <Route path="/contact" element={ < Contact /> } />
-                    <Route path="*" element={<div><h1>ERROR 404</h1><h2>Taka strona nie istnieje</h2> </div> } /> 
-                </Route>
-            </Routes>
-        );
-    }
+  render() {
+    return (
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/caloriesCounter" element={<CaloriesCounter />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>ERROR 404</h1>
+                <h2>Taka strona nie istnieje</h2>{" "}
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    );
+  }
 }
 
 export default App;
