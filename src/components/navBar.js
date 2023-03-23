44dc6e5f0277d72425b43485430e542e3613f308
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-23 23:21:45 +0100

    adding logo

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../images/logo.jpg"
+   
 function NavBar({ children }) {
   return (
     <>
       <div className="navBar">
+        <img src={logo} alt="logo" width="100" height="100"/>
         <div className="buttons">
           <Link to="/">HomePage</Link>
           <Link to="/caloriesCounter">Calories Counter</Link>
