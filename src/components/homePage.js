8319e123b788c575fd0dad4f370207a446bc7961
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-05 21:21:34 +0200

    creating style

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Footer from "./footer";
+import salad1 from "../images/salad1.jpg"
 
 function HomePage() {
   return (
     <>
     <div className="homePage">
         <div className="heading1">
-            <span>Welcome to A Tu Fit website</span>
+            <span>YOUR DAILY ASSISTANT</span>
           <div className="heading2">
             <span>
-              A step-by-step guide through the process of creating your diet.
-              Our team has developed a simple and easy to use diet diary
-              builder. It is powered by a constantly expanding range of new and
-              proven products.
+            A step-by-step guide through the process of creating a diet. Know your daily calorie and macronutrient needs and then in the count calories tab, add the products you consumed during the day and compare the result to your needs
             </span>
+            <div className="homePageIcon1">
+            <img className="salad1" src={salad1} alt="salad1" />
+            </div>
           </div>
         </div>
       </div>
