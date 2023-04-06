3533fb3a00c4a0952ad730e871b901f2605a8a00
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-06 21:39:37 +0200

    adding style to homepage

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,25 +1,26 @@
 import React from "react";
 import Footer from "./footer";
 import salad1 from "../images/salad1.jpg"
+import vegetables from "../images/vegetables.jpg"
 
 function HomePage() {
   return (
     <>
     <div className="homePage">
-        <div className="heading1">
+            <div className="homePageIcon1">
+            <img className="salad1" src={salad1} alt="salad1" />
+            </div>
+            <div className="heading1">
             <span>YOUR DAILY ASSISTANT</span>
           <div className="heading2">
             <span>
             A step-by-step guide through the process of creating a diet. Know your daily calorie and macronutrient needs and then in the count calories tab, add the products you consumed during the day and compare the result to your needs
             </span>
-            <div className="homePageIcon1">
-            <img className="salad1" src={salad1} alt="salad1" />
-            </div>
             <div className="heading3">
               <span>Text text text</span>
             </div>
             <div className="homePageIcon2">
-              
+            <img className="vegetables" src={vegetables} alt="vegetables" />
             </div>
           </div>
         </div>
