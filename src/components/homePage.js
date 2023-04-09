15781a69c7d5ee9d0f9cc89db89ae073042895ad
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-10 00:45:19 +0200

    creating homePage style

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -8,23 +8,23 @@ function HomePage() {
     <>
       <div className="homePage">
         <div className="firstSlide">
+        <img className="salad1" src={salad1} alt="salad1" />
           <div className="heading1">
             <span>YOUR DAILY ASSISTANT</span>
           </div>
           <div className="heading2">
             <span>
-              A step-by-step guide through the process of creating a diet. Know
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.//A step-by-step guide through the process of creating a diet. Know
               your daily calorie and macronutrient needs and then in the count
               calories tab, add the products you consumed during the day and
               compare the result to your needs
             </span>
           </div>
-          <img className="salad1" src={salad1} alt="salad1" />
         </div>
         <div className="secondSlide">
           <img className="vegetables" src={vegetables} alt="vegetables" />
           <div className="heading3">
-            <span>Text text text</span>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
           </div>
         </div>
       </div>
