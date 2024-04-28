f6802bdde1af09d1757409fd98befc10bbddecdb
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-28 14:16:05 +0200

    connect express with front

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import FetchData from "./FetchData";
+import React, { useState } from "react";
+import axios from "axios";
 import "../styles/AddProduct.css";
+import { useEffect } from "react";
 
 const AddProduct = () => {
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/hello");
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  
   return (
-    <div className="add-product">
-      <h3>Not enough? Add your own product</h3>
-      <FetchData />
-      <FetchData url="http://localhost:3001/hello" />
-      <FetchData url="http://localhost:3001/users" />
-      <FetchData url="http://localhost:3001/products" />
-    </div>
+    <>
+      <div className="add-product">
+        <h3>Not enough? Add your own product</h3>
+        <div className="api">{data}</div>
+      </div>
+    </>
   );
 };
 
