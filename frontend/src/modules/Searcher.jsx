6512eebccdfc4e9ea9c13db7b69704fbf8834e90
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-05-14 22:41:39 +0200

    uptodate

--- frontend/src/modules/Searcher.jsx
+++ frontend/src/modules/Searcher.jsx
@@ -20,13 +20,17 @@ class Searcher extends React.Component {
             this.state.products +
             "&addProductInformation=true&apiKey=6d0d470152d74ee2aa61eaa38e37af8d"
         ),
-        axios.get("http://localhost:3001/api/getProducts/" + this.state.products),
+        axios.get("http://localhost:3001/api/getProducts/", {
+          params: {
+            product: this.state.product,
+          },
+        }),
       ])
       .then(
         axios.spread((response1, response2) => {
           let recipes1 = response1.data.products;
           let recipes2 = response2.data.products;
-  
+
           if (recipes1.length === 0) {
             this.setState({
               recipes: recipes2,
@@ -34,7 +38,7 @@ class Searcher extends React.Component {
           } else {
             this.setState({
               recipes: recipes1,
-              recipes2: recipes2, 
+              recipes2: recipes2,
             });
           }
         })
@@ -80,24 +84,16 @@ class Searcher extends React.Component {
                 <div className="row">
                   <div className="col">{this.state.recipes[key].title}</div>
                   <div className="col">
-                    {this.roundToNearestPrime(
-                      parseFloat(this.state.recipes[key].nutrition.calories)
-                    )}
+                    {this.state.recipes[key].nutrition.calories}
                   </div>
                   <div className="col">
-                    {this.roundToNearestPrime(
-                      parseFloat(this.state.recipes[key].nutrition.fat)
-                    )}
+                    {this.state.recipes[key].nutrition.fat}
                   </div>
                   <div className="col">
-                    {this.roundToNearestPrime(
-                      parseFloat(this.state.recipes[key].nutrition.carbs)
-                    )}
+                    {this.state.recipes[key].nutrition.carbs}
                   </div>
                   <div className="col">
-                    {this.roundToNearestPrime(
-                      parseFloat(this.state.recipes[key].nutrition.protein)
-                    )}
+                    {this.state.recipes[key].nutrition.protein}
                   </div>
                   <div className="col-md-2">
                     <select
@@ -114,7 +110,9 @@ class Searcher extends React.Component {
                     >
                       <option value={"choose part of day"}>Choose</option>
                       <option value={"breakfast"}>Breakfast</option>
-                      <option value={"secondBreakfast"}>Second Breakfast</option>
+                      <option value={"secondBreakfast"}>
+                        Second Breakfast
+                      </option>
                       <option value={"dinner"}>Dinner</option>
                       <option value={"supper"}>Supper</option>
                     </select>
@@ -140,7 +138,9 @@ class Searcher extends React.Component {
                   <div className="col">{this.state.recipes[key].name}</div>
                   <div className="col">{this.state.recipes[key].calories}</div>
                   <div className="col">{this.state.recipes[key].fats}</div>
-                  <div className="col">{this.state.recipes[key].carbohydrates}</div>
+                  <div className="col">
+                    {this.state.recipes[key].carbohydrates}
+                  </div>
                   <div className="col">{this.state.recipes[key].proteins}</div>
                   <div className="col-md-2">
                     <select
@@ -157,7 +157,9 @@ class Searcher extends React.Component {
                     >
                       <option value={"choose part of day"}>Choose</option>
                       <option value={"breakfast"}>Breakfast</option>
-                      <option value={"secondBreakfast"}>Second Breakfast</option>
+                      <option value={"secondBreakfast"}>
+                        Second Breakfast
+                      </option>
                       <option value={"dinner"}>Dinner</option>
                       <option value={"supper"}>Supper</option>
                     </select>
