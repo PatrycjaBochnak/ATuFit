e013989720edbb0a8d98b5cba270d2a1e13edbfc
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2025-01-23 23:08:10 +0100

    Uptodate

--- frontend/src/layout/NavMenu.jsx
+++ frontend/src/layout/NavMenu.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import NavButtons from "../components/NavButtons";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-900 shadow-md z-50">
@@ -23,28 +21,42 @@ const NavMenu = () => {
         </div>
         {/* Mobile Menu Toggle */}
         <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-gray-300 focus:outline-none"
-          >
             {/* Hamburger Icon */}
-            {isOpen ? "✕" : "☰"}
-          </button>
+            <div
+              onClick={handleClick}
+              className="absolute top-4 right-4 text-2xl text-gray-300 cursor-pointer z-50"
+            >
+              {!nav ? <FaBars /> : <FaTimes />}
+            </div>
         </div>
       </div>
 
       {/* Mobile Menu */}
       <div
         className={`${
-          isOpen ? "block" : "hidden"
-        } md:hidden bg-gray-900 text-gray-300 space-y-4 py-4`}
+          nav ? "block" : "hidden"
+        } md:hidden bg-gray-900 text-gray-300 bg-opacity-100 z-40 flex flex-col items-center h-screen px-4`}
       >
-        <NavButtons source="home-page" linkName="Start" />
-        <NavButtons source="calculator-BMR" linkName="Calculator" />
-        <NavButtons source="counter" linkName="Calories counter" />
-        <NavButtons source="add-product" linkName="Add product" />
-        <NavButtons source="daily-results" linkName="Daily results" />
-        <NavButtons source="contact" linkName="Contact" />
+        <div className="text-2xl text-white flex flex-col items-center space-y-6">
+          <button className="w-full py-2 text-center">
+            <NavButtons source="home-page" linkName="Start" />
+          </button>
+          <button className="w-full py-2 text-center">
+            <NavButtons source="calculator-BMR" linkName="Calculator" />
+          </button>
+          <button className="w-full py-2 text-center">
+            <NavButtons source="counter" linkName="Calories counter" />
+          </button>
+          <button className="w-full py-2 text-center">
+            <NavButtons source="add-product" linkName="Add product" />
+          </button>
+          <button className="w-full py-2 text-center">
+            <NavButtons source="daily-results" linkName="Daily results" />
+          </button>
+          <button className="w-full py-2 text-center">
+            <NavButtons source="contact" linkName="Contact" />
+          </button>
+        </div>
       </div>
     </nav>
   );
